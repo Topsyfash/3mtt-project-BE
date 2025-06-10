4e1150d54ef449ab217524674b8807580913d827
fasogba ayomide isaac <ayomidefasogba@gmail.com>
2025-06-10 20:02:38 +0100

    updated

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,14 +1,14 @@
 import FavoriteMovie from "../models/favoriteMovieModel.js"
 import tmdb from "../utils/tmdb.js"
 
-const searchMovies = async (req,res) => {
+const searchMovies = async (req, res) => {
     try {
         const { query, year } = req.query
 
-         if (!query) {
-      return res.status(400).json({ message: "Query parameter is required." });
-    }
-        
+        if (!query) {
+            return res.status(400).json({ message: "Query parameter is required." });
+        }
+
         const response = await tmdb.get('/search/movie', {
             params: {
                 query,
@@ -17,19 +17,19 @@ const searchMovies = async (req,res) => {
         })
 
         let movies = response.data.results;
-        
+
         movies.sort((a, b) => {
-      const dateA = new Date(a.release_date);
-      const dateB = new Date(b.release_date);
-      return dateB - dateA;
+            const dateA = new Date(a.release_date);
+            const dateB = new Date(b.release_date);
+            return dateB - dateA;
         });
-        
+
         res.status(200).json({
             message: "Sucessful",
-            movies:movies
+            movies: movies
         })
-    } catch(error) {
-        res.status(500).json({message:error.message})
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 }
 
@@ -39,51 +39,51 @@ const getPopularMovies = async (req, res) => {
         const response = await tmdb.get("/movie/popular")
         res.status(200).json({
             message: "Sucessful",
-            movies:response.data.results
+            movies: response.data.results
         })
     } catch (error) {
-        res.status(500).json({message:error.message,error: error?.response?.data || error.message})
+        res.status(500).json({ message: error.message, error: error?.response?.data || error.message })
     }
 }
- 
+
 
 const getMovieInfo = async (req, res) => {
-    
+
     try {
         const { id } = req.params
         const response = await tmdb.get(`/movie/${id}`)
-        
+
         res.status(200).json({
             message: "Sucessful",
-            movie:response.data
+            movie: response.data
         })
     } catch (error) {
-        res.status(500).json({message:error.message})
+        res.status(500).json({ message: error.message })
     }
 
 }
 
 const getMovieTrailer = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { data } = await tmdb.get(
-      `/movie/${id}/videos?language=en-US` 
-    );
+    try {
+        const { id } = req.params;
+        const { data } = await tmdb.get(
+            `/movie/${id}/videos?language=en-US`
+        );
 
-    const trailer = data.results.find(
-      (v) => v.type === 'Trailer' && v.site === 'YouTube'
-    );
+        const trailer = data.results.find(
+            (v) => v.type === 'Trailer' && v.site === 'YouTube'
+        );
 
-       if (!trailer) {
-      return res.status(404).json({ message: 'Trailer not found' });
+        if (!trailer) {
+            return res.status(404).json({ message: 'Trailer not found' });
+        }
+        res.json({ trailer });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Failed to fetch trailer',
+            error
+        });
     }
-    res.json({ trailer });
-  } catch (error) {
-      res.status(500).json({
-          message: 'Failed to fetch trailer',
-          error
-      });
-  }
 }
 
 
@@ -92,40 +92,38 @@ const getMovieRecommendations = async (req, res) => {
     try {
         const favorites = await FavoriteMovie.find({ userId: req.user.id });
 
-        console.log(favorites)
 
         const favoriteIds = favorites.map((fav) => fav.movieId);
-        console.log(favoriteIds)
 
-    let recommendedMovies = [];
+        let recommendedMovies = [];
 
         for (const movieId of favoriteIds.slice(0, 5)) {
-         if (!movieId || isNaN(movieId)) {
-    console.warn(`Invalid TMDB movieId:`, movieId);
-    continue;
-  }
-      const response = await tmdb.get(`/movie/${movieId}/recommendations`);
-      recommendedMovies.push(...response.data.results);
-    }
+            if (!movieId || isNaN(movieId)) {
+                console.warn(`Invalid TMDB movieId:`, movieId);
+                continue;
+            }
+            const response = await tmdb.get(`/movie/${movieId}/recommendations`);
+            recommendedMovies.push(...response.data.results);
+        }
 
-    const seen = new Set();
-    const uniqueMovies = recommendedMovies.filter((movie) => {
-      if (seen.has(movie.id)) return false;
-      seen.add(movie.id);
-      return true;
-    });
+        const seen = new Set();
+        const uniqueMovies = recommendedMovies.filter((movie) => {
+            if (seen.has(movie.id)) return false;
+            seen.add(movie.id);
+            return true;
+        });
 
-    res.status(200).json({
-      message: "Personalized recommendations",
-      movies: uniqueMovies.slice(0, 20),
-    });
+        res.status(200).json({
+            message: "Personalized recommendations",
+            movies: uniqueMovies.slice(0, 20),
+        });
     } catch (error) {
         console.error(" Axios or TMDB error:", error);
-    res.status(500).json({ message: error.message });
-  }
+        res.status(500).json({ message: error.message });
+    }
 };
 
 
 
 
-export {searchMovies,getPopularMovies,getMovieInfo , getMovieTrailer,getMovieRecommendations}
+export { searchMovies, getPopularMovies, getMovieInfo, getMovieTrailer, getMovieRecommendations }
